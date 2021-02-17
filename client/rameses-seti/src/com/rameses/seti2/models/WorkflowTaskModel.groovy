8b059d2346c623548af67333e8583350d60cb952
elmonazareno <elmonazareno@gmail.com>
2021-02-17 17:04:38 +0800

    updated workflow

--- client/rameses-seti/src/com/rameses/seti2/models/WorkflowTaskModel.groovy
+++ client/rameses-seti/src/com/rameses/seti2/models/WorkflowTaskModel.groovy
@@ -76,7 +76,7 @@ public class WorkflowTaskModel extends CrudFormModel implements WorkflowTaskList
         return procName;
     }
 
-    public def open() {
+    public def open() {        
         //we need to do this so it will not affect the original entity from the list.
         def tsk = null;
         def n = entity;
@@ -146,6 +146,7 @@ public class WorkflowTaskModel extends CrudFormModel implements WorkflowTaskList
     }
     
     final void buildTransitionActions( def tsk ) {
+         transitions.clear();        
          if(! tsk) return; 
          if( tsk.state == 'end' ) return; 
          if( !tsk.assignee?.objid && tsk.role != null ) {
